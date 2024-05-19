42bfcdf2c965eae97d7c7e9256ea706de60dffe8
Erick Escobar <ErickD.EscobarMelgar@gmail.com>
2024-05-18 18:07:50 -0600

    Actividad 5

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 const router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/desarrolloweb')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usersTasks = require('./routes/tasks');
 var usersGoals = require('./routes/goals');
+
 
 var app = express();
 
